3f79e1cbdaa9971805c060d85fab42dcf749a047
Mugil <mugil15112001@gmail.com>
2026-08-03 13:03:35 +0530

    Update javascript.js

---
+++ my-project/javascript.js
@@ -0,0 +1,4 @@
+const name = "Alice";      // String (text)
+let userAge = 25;          // Number
+const isLogggedIn = true;  // Boolean (true/false)
+const skills = ["HTML", "CSS", "JS"]; // Array (list)
